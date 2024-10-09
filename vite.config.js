66a98880195f129e52907acdf00ec8ef12164233
Daksh-18 <dakshyadav2003@gmail.com>
2024-10-09 15:39:24 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // build: {
-  //   outDir: 'build', // Specify the output directory
-  // },
+  build: {
+    outDir: 'dist', // Specify the output directory
+  },
   base: '/book-website/',
 })
